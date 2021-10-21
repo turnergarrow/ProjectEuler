b8e49d2c80544b408c02f627523b918bc8a0adc6
turnergarrow <tugarrow15@gmail.com>
2021-10-21 11:58:42 -0400

    Solved problem 719

--- PE719.py
+++ PE719.py
@@ -1,6 +1,30 @@
-sqr = []
+from euler import *
 
-n = 100
+# NOT a fast solution, takes longer than the time limit
+# Finds a solution eventually
 
-def is_S(n):
-    str_n = str(n)
+n = int(1e6)
+
+def is_S(m, n):
+
+    if (m < n) or n <= 0: return False
+    if (m == n): return True
+
+    t = 10
+    while (t < m):
+        div, mod = divmod(m, t)
+        if is_S(div, n-mod):
+            return True
+        t = t*10
+
+    return False
+
+
+if __name__ == "__main__":
+    tot = 0
+    for i in range(2, n+1):
+        sqr = i*i
+        if is_S(sqr, i):
+            print(i, sqr)
+            tot += sqr
+    print(tot)
